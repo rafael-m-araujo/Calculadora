a86776633c41b39acec82c69a3638ccae213c449
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-15 20:49:12 -0300

    Commit quase fim da calculadora

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+
+
+// Funções
+const createTable = (number, multiplicationNumber) => {
+
+
+}
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+     if(!multiplicationNumber || !multiplicationNumber) return;
+
+    console.log(multiplicationNumber, multiplicatorNumber);
+});
