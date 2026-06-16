21daee8f3dbfcc791fc63f095a0dca1a7e1a38cb
Rafael Mendes Araújo <rafaelmendes0006@gmail.com>
2026-06-16 05:58:16 -0300

    Finalização Do Projeto Calculadora!

--- js/script.js
+++ js/script.js
@@ -3,13 +3,37 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
+const multiplicationTitle = document.querySelector("#multiplication-title span");
 
+const multiplicationTable = document.querySelector(
+    "#multiplication-operations"
+);
 
 // Funções
-const createTable = (number, multiplicationNumber) => {
+const createTable = (number, multiplicatorNumber) => {
+   multiplicationTable.innerHTML = "";
+
+   for(i = 1; i <= multiplicatorNumber; i++) {
+
+  const result = number * i;
 
 
-}
+const template = `<div class="row">
+<div class="operation">${number} x ${i} = </div>
+<div class="result">${result}</div>
+</div>`;
+
+const parser = new DOMParser();
+
+const htmlTemplate = parser.parseFromString(template, "text/html");
+
+const row = htmlTemplate.querySelector(".row");
+
+multiplicationTable.appendChild(row);
+   }
+
+   multiplicationTitle.innerText = number;
+};
 
 // Eventos
 multiplicationForm.addEventListener("submit", (e) => {
@@ -19,7 +43,7 @@ multiplicationForm.addEventListener("submit", (e) => {
 
     const multiplicatorNumber = +multiplicationInput.value;
 
-     if(!multiplicationNumber || !multiplicationNumber) return;
+     if(!multiplicationNumber || !multiplicatorNumber) return;
 
-    console.log(multiplicationNumber, multiplicatorNumber);
+    createTable(multiplicationNumber, multiplicatorNumber);
 });
